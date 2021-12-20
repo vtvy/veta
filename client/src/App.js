25a8edd2d1f7953a5226c8a2cba659dd63ebc6ba
votrieuvy1661 <votrieuvy1661@gmail.com>
2021-12-20 23:38:50 +0700

    fix login logup

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
-import Auth from './features/Auth';
-import { Routes, Route } from 'react-router-dom';
-import Test from './test';
-import SignInPage from './features/Auth/Pages/SignInPage';
-import RegisterPage from './features/Auth/Pages/RegisterPage';
-import TestServer from './testServer';
+import Auth from "./features/Auth";
+import { Routes, Route } from "react-router-dom";
+import Test from "./test";
+import SignInPage from "./features/Auth/Pages/SignInPage";
+import RegisterPage from "./features/Auth/Pages/RegisterPage";
+import TestServer from "./testServer";
 
 function App() {
-	return (
-		<div className="App h-screen">
-			<Routes>
-				<Route path="/test" element={<Test />} />
-				<Route path="/test" element={<TestServer />} />
-				<Route path="/login" element={<SignInPage />} />
-				<Route path="/register" element={<RegisterPage />} />
-			</Routes>
-		</div>
-	);
+  return (
+    <div className="App h-screen">
+      <Routes>
+        <Route path="/test" element={<Test />} />
+        <Route path="/test" element={<TestServer />} />
+        <Route path="/login" element={<SignInPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,167 +1,172 @@
-const express = require('express');
-const nodemailer = require('nodemailer');
+const express = require("express");
+const nodemailer = require("nodemailer");
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
-const jwt = require('jsonwebtoken');
-const fs = require('fs');
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
 
 const otp = parseInt(Math.random() * 10000);
 
-const User = require('../models/User');
+const User = require("../models/User");
 
 const transporter = nodemailer.createTransport({
-	host: 'smtp.gmail.com',
-	port: 465,
-	secure: true,
-	service: 'Gmail',
-	auth: {
-		user: 'vnteamnoreply@gmail.com',
-		pass: 's@oslpVN',
-	},
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  service: "Gmail",
+  auth: {
+    user: "vnteamnoreply@gmail.com",
+    pass: "s@oslpVN",
+  },
 });
 
 //send otp
-router.post('/otp', async (req, res) => {
-	console.log(1);
-	const email = req.body.email;
-	try {
-		//Check for exists
-		const existUser = await User.findOne({ email });
+router.post("/otp", async (req, res) => {
+  console.log(1);
+  const email = req.body.email;
 
-		if (existUser) {
-			return res.json({
-				success: false,
-				message: 'This email has been registered',
-			});
-		} else {
-			let mailOptions = {
-				from: 'vnteamnoreply@gmail.com',
-				to: email,
-				subject: 'Otp for registration is: ',
-				html:
-					'<h3>OTP for account verification is </h3>' +
-					"<h1 style='font-weight:bold;'>" +
-					otp +
-					'</h1>',
-			};
-			transporter.sendMail(mailOptions, (error, info) => {
-				if (error) {
-					return console.log(error);
-				} else {
-					console.log('Message sent: %s', info.response);
-					console.log(2);
+  try {
+    //Check for exists
+    const existUser = await User.findOne({ email });
 
-					return res.json({
-						success: true,
-						message: 'OTP has been send',
-					});
-				}
-			});
-		}
-	} catch (error) {
-		console.log(error);
-	}
+    console.log(existUser);
+    if (existUser) {
+      return res.json({
+        success: false,
+        message: "This email has been registered",
+      });
+    } else {
+      let mailOptions = {
+        from: "vnteamnoreply@gmail.com",
+        to: email,
+        subject: "Otp for registration is: ",
+        html:
+          "<h3>OTP for account verification is </h3>" +
+          "<h1 style='font-weight:bold;'>" +
+          otp +
+          "</h1>",
+      };
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          return console.log(error);
+        } else {
+          console.log("Message sent: %s", info.response);
+          console.log(2);
+
+          return res.json({
+            success: true,
+            message: "OTP has been send",
+          });
+        }
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  console.log(4);
 });
 
-router.post('/confirmOtp', async (req, res) => {
-	const user = req.body;
-
-	if (user.otp == otp) {
-		res.json({ success: true, message: 'OTP is correct' });
-	} else {
-	}
+router.post("/confirmOtp", async (req, res) => {
+  const user = req.body;
+  console.log(user);
+  if (user.otp == otp) {
+    res.json({ success: true, message: "OTP is correct" });
+  } else {
+    res.json({ success: false, message: "OTP is incorrect" });
+  }
 });
 //Register an account
-router.post('/register', async (req, res) => {
-	console.log(3);
+router.post("/register", async (req, res) => {
+  const user = req.body;
+  var avatarPath = "";
+  let a = user.isDefault;
+  if (user.isDefault === true) {
+    avatarPath =
+      user.email.slice(0, 5) + "_" + Date.now().toString() + user.avatar;
 
-	const user = req.body;
-	var avatarPath = '';
-	console.log(req.files);
+    fs.copyFile(
+      `../client/src/assets/images/avatars/${user.avatar}`,
+      `../client/src/assets/uploads/avatars/${avatarPath}`,
+      (err) => {
+        console.log(err);
+      }
+    );
+  } else {
+    const file = req.files.avatar;
+    console.log(file.name);
+    avatarPath =
+      user.email.slice(0, 5) + "_" + Date.now().toString() + file.name;
+    file.mv(`../client/src/assets/uploads/avatars/${avatarPath}`, (err) => {
+      console.error(err);
+    });
+  }
 
-	if (user.isDefault) {
-		avatarPath = user.firstName + Date.now + user.avatar;
-		console.log(avatarPath);
+  //Create new account
+  console.log(avatarPath);
+  try {
+    const hash = bcrypt.hashSync(user.password, saltRounds);
+    const newUser = new User({
+      email: user.email,
+      password: hash,
+      avatar: avatarPath,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      birthDate: user.birthDate,
+    });
+    await newUser.save();
 
-		fs.copyFile(
-			`../../client/src/assets/images/avatars/${user.avatar}`,
-			`../../client/src/assets/uploads/avatars/${avatarPath}`,
-			(err) => {
-				console.log(err);
-			}
-		);
-	} else {
-		const file = req.files.file;
-		avatarPath = user.firstName + Date.now + file.name;
-		console.log(avatarPath);
-		file.mv(`../../client/src/assets/uploads/avatars/${avatarPath}`, (err) => {
-			console.error(err);
-		});
-	}
+    //Return token
+    const accessToken = jwt.sign(
+      { userID: newUser._id },
+      process.env.ACCESS_TOKEN_CODE
+    );
 
-	//Create new account
-	console.log(avatarPath);
-
-	const hash = bcrypt.hashSync(user.password, saltRounds);
-	const newUser = new User({
-		email: user.email,
-		password: hash,
-		avatar: avatarPath,
-		firstName: user.firstName,
-		lastName: user.lastName,
-		birthDate: user.birthDate,
-	});
-	await newUser.save();
-
-	//Return token
-	const accessToken = jwt.sign(
-		{ userID: newUser._id },
-		process.env.ACCESS_TOKEN_CODE
-	);
-
-	res.json({ success: true, message: 'Register successfully', accessToken });
-	console.log(4);
+    res.json({ success: true, message: "Register successfully", accessToken });
+  } catch (e) {
+    res.json({ success: false, message: "You need to input all fields" });
+  }
 });
 
 //Login
-router.post('/login', async (req, res) => {
-	const { email, password } = req.body;
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
 
-	try {
-		//Check for exists
-		const existUser = await User.findOne({ email });
+  try {
+    //Check for exists
+    const existUser = await User.findOne({ email });
 
-		if (!existUser) {
-			return res.json({
-				success: false,
-				message: 'Invalid account',
-			});
-		} else {
-			const match = bcrypt.compareSync(password, existUser.password);
-			if (!match) {
-				return res.json({
-					success: false,
-					message: 'Invalid account',
-				});
-			} else {
-				const accessToken = jwt.sign(
-					{ userID: existUser._id },
-					process.env.ACCESS_TOKEN_CODE
-				);
+    if (!existUser) {
+      return res.json({
+        success: false,
+        message: "Invalid account",
+      });
+    } else {
+      const match = bcrypt.compareSync(password, existUser.password);
+      if (!match) {
+        return res.json({
+          success: false,
+          message: "Invalid account",
+        });
+      } else {
+        //Return token
+        const accessToken = jwt.sign(
+          { userID: existUser._id },
+          process.env.ACCESS_TOKEN_CODE
+        );
 
-				res.json({
-					success: true,
-					message: 'Login successfully',
-					accessToken,
-				});
-			}
-		}
-	} catch (error) {
-		console.log(error);
-	}
+        res.json({
+          success: true,
+          message: "Login successfully",
+          accessToken,
+        });
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-router.get('/', (req, res) => res.send('User'));
+router.get("/", (req, res) => res.send("User"));
 
 module.exports = router;
