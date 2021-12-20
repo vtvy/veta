a115696e871abf86073b8a5d04fa24f9d6e00da8
votrieuvy1661 <votrieuvy1661@gmail.com>
2021-12-20 16:41:53 +0700

    done backend s

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import Test from "./test";
 import SignInPage from "./features/Auth/Pages/SignInPage";
 import SignUpPage from "./features/Auth/Pages/SignUpPage";
+import TestServer from "./testServer";
 
 function App() {
   return (
     <div className="App h-screen">
       <Routes>
         <Route path="/test" element={<Test />} />
+        <Route path="/test" element={<TestServer />} />
         <Route path="/login" element={<SignInPage />} />
         <Route path="/register" element={<SignUpPage />} />
       </Routes>
