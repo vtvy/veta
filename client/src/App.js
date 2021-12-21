7ea0c3477a87b806026ded97cb8b2fb05c94239c
NguyenMinhNhat <nguyenminhnhat301101@gmail.com>
2021-12-21 07:59:43 +0700

    fix bug Protected route

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,15 @@ import Auth from './features/Auth';
 import { Routes, Route } from 'react-router-dom';
 import Test from './test';
 import TestServer from './testServer';
-import ProtectedRoutes from './components/header/ProtectedRoutes';
+import ProtectedRoutes from './components/ProtectedRoutes';
 import RegisterPage from './features/Auth/Pages/RegisterPage';
-
 function App() {
 	return (
 		<div className="App h-screen">
-			<Auth />
 			<Routes>
-				<Route element={<ProtectedRoutes isLogged={true} />}>
+				<Route path="/login" element={<Auth type="login" />} />
+				<Route path="/register" element={<Auth type="register" />} />
+				<Route element={<ProtectedRoutes isLogged={false} />}>
 					<Route path="/registers" element={<RegisterPage />} />
 					<Route path="/test" element={<TestServer />} />
 					<Route path="/test" element={<Test />} />
--- client/src/features/Auth/index.js
+++ client/src/features/Auth/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import RegisterPage from './Pages/RegisterPage';
 import LoginPage from './Pages/LoginPage';
-import { Route, Routes } from 'react-router-dom';
 
-function Auth() {
+function Auth({ type }) {
 	return (
 		<div className="w-full h-full flex justify-center items-center bg-slate-600">
-			<Routes>
-				<Route path="/login" element={<LoginPage />} />
-				<Route path="/register" element={<RegisterPage />} />
-			</Routes>
+			{type === 'login' ? <LoginPage /> : <RegisterPage />}
 		</div>
 	);
 }
