44bb674157109d3e98770727ed4a89b9ec2c3e38
votrieuvy1661 <votrieuvy1661@gmail.com>
2021-12-22 15:35:41 +0700

    get .env

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,25 +1,18 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
-	console.log(req);
-	const token = req.header('accessToken');
-	console.log(token);
-	if (!token)
-		return res
-			.status(401)
-			.json({ success: false, message: 'User not log in!' });
+  console.log(req);
+  const token = req.header("accessToken");
+  console.log(token);
+  if (!token) return res.json({ success: false, message: "User not log in!" });
 
-	try {
-		const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_CODE);
-		req.userID = decoded.userID;
-		req.email = decoded.email;
-		next();
-	} catch (error) {
-		console.log(error);
-		return res
-			.status(403)
-			.json({ success: false, message: 'User not log in!' });
-	}
+  try {
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_CODE);
+    req.body.userID = decoded.userID;
+    next();
+  } catch (error) {
+    return res.json({ success: false, message: "User not log in!", error });
+  }
 };
 
 module.exports = verifyToken;
--- server/routes/post.js
+++ server/routes/post.js
@@ -1,44 +1,52 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const Post = require('../models/Post');
-const verifyToken = require('../middleware/auth');
+const Post = require("../models/Post");
+const verifyToken = require("../middleware/auth");
 
-router.post('/create', verifyToken, async (req, res) => {
-	const post = req.body;
+router.post("/create", verifyToken, async (req, res) => {
+  const post = req.body;
 
-	console.log(post.postText !== '');
-	if (post.postImage === undefined || post.postText !== '') {
-		try {
-			if (req.files.postImage) {
-				const file = req.files.postImage;
-				var filePath =
-					req.email.slice(0, 5) + '_' + Date.now().toString() + file.name;
-				file.mv(`../client/public/assets/uploads/posts/${filePath}`, (err) => {
-					console.error(err);
-				});
-			}
+  console.log(post.postText !== "");
+  if (post.postImage === undefined || post.postText !== "") {
+    try {
+      if (req.files.postImage) {
+        const file = req.files.postImage;
+        var filePath =
+          req.email.slice(0, 5) + "_" + Date.now().toString() + file.name;
+        file.mv(`../client/public/assets/uploads/posts/${filePath}`, (err) => {
+          console.error(err);
+        });
+      }
 
-			//Create new post
-			const newPost = new Post({
-				postText: post.postText,
-				postImage: filePath,
-			});
-			await newPost.save();
-			return res.json({
-				success: true,
-				message: 'Post a status successfully',
-				newPost,
-			});
-		} catch (e) {
-			console.log(e);
-			return res.json({ success: false, message: 'You need to input a value' });
-		}
-		// console.log(post.postText);
-		// console.log(post.postImage);
-		console.log(!(post.postText && post.postImage));
-	}
-	return res.json({ success: false, message: 'You need to input something' });
+      //Create new post
+      const newPost = new Post({
+        postText: post.postText,
+        postImage: filePath,
+        user: req.userID,
+      });
+      await newPost.save();
+      return res.json({
+        success: true,
+        message: "Post a status successfully",
+        newPost,
+      });
+    } catch (error) {
+      return res.json({
+        success: false,
+        message: "You need to input a value",
+        error,
+      });
+    }
+  }
+  return res.json({ success: false, message: "You need to input something" });
+});
+
+router.get("/", verifyToken, async (req, res) => {
+  const { userID } = req.body;
+  console.log("o");
+  const listOfPost = Post.findAll({ user: userID });
+  res.json({ success: true, message: "This is list of post", listOfPost });
 });
 
 module.exports = router;
