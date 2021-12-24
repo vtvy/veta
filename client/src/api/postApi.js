d8e50a31370fe1e7fe348e4901b045bb2bbd71c8
votrieuvy1661 <votrieuvy1661@gmail.com>
2021-12-24 11:07:41 +0700

    done 2 func

--- client/src/api/postApi.js
+++ client/src/api/postApi.js
@@ -1,39 +1,48 @@
-import StorageKeys from '../constants/storageKeys';
-import axiosClient from './axiosClient';
+import StorageKeys from "../constants/storageKeys";
+import axiosClient from "./axiosClient";
 const accessToken = localStorage.getItem(StorageKeys.accessToken);
 
 const postApi = {
-	create(data) {
-		const url = '/post/create';
+  create(data) {
+    const url = "/post/create";
 
-		return axiosClient.post(url, data, {
-			headers: { accessToken },
-		});
-	},
+    return axiosClient.post(url, data, {
+      headers: { accessToken },
+    });
+  },
 
-	async getAll() {
-		const url = '/post';
-		const accessToken = localStorage.getItem(StorageKeys.accessToken);
-		const res = await axiosClient.get(url, {
-			headers: { accessToken },
-		});
-		return res;
-	},
-	async get(id) {
-		const url = `/posts/${id}`;
+  async getAll() {
+    const url = "/post";
+    const accessToken = localStorage.getItem(StorageKeys.accessToken);
+    const res = await axiosClient.get(url, {
+      headers: { accessToken },
+    });
+    return res;
+  },
+  async get(id) {
+    const url = `/posts/${id}`;
 
-		const res = await axiosClient.get(url, {
-			headers: { accessToken },
-		});
-		return res;
-	},
-	async deletePostById(id) {
-		const url = `/post/delete/${id}`;
-		const res = await axiosClient.delete(url, {
-			headers: { accessToken },
-		});
-		return res;
-	},
+    const res = await axiosClient.get(url, {
+      headers: { accessToken },
+    });
+    return res;
+  },
+
+  async updatePostById(id, data) {
+    const url = `/post/update/${id}`;
+    const res = await axiosClient.put(url, data, {
+      headers: { accessToken },
+    });
+    return res;
+  },
+
+  async deletePostById(id) {
+    const url = `/post/delete/${id}`;
+    const res = await axiosClient.delete(url, {
+      headers: { accessToken },
+    });
+    return res;
+  },
 };
 
 export default postApi;
--- server/routes/post.js
+++ server/routes/post.js
@@ -1,126 +1,142 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const fs = require('fs');
+const fs = require("fs");
 
-const Post = require('../models/Post');
-const verifyToken = require('../middleware/auth');
+const Post = require("../models/Post");
+const verifyToken = require("../middleware/auth");
 
-router.post('/create', verifyToken, async (req, res) => {
-	const post = req.body;
-	if (post.postImage === undefined || post.postText !== '') {
-		try {
-			var filePath = '';
-			if (req.files?.postImage) {
-				const file = req.files.postImage;
-				filePath =
-					post.email.slice(0, 5) + '_' + Date.now().toString() + file.name;
-				file.mv(`../client/public/assets/uploads/posts/${filePath}`, (err) => {
-					console.error(err);
-				});
-			}
+router.post("/create", verifyToken, async (req, res) => {
+  const post = req.body;
+  if (post.postImage === undefined || post.postText !== "") {
+    try {
+      var filePath = "";
+      if (req.files?.postImage) {
+        const file = req.files.postImage;
+        filePath =
+          post.email.slice(0, 5) + "_" + Date.now().toString() + file.name;
+        file.mv(`../client/public/assets/uploads/posts/${filePath}`, (err) => {
+          console.error(err);
+        });
+      }
 
-			//Create new post
-			const newPost = new Post({
-				postText: post.postText,
-				postImage: filePath,
-				user: post.userID,
-			});
-			await newPost.save();
-			return res.json({
-				success: true,
-				message: 'Post a status successfully',
-				newPost,
-			});
-		} catch (error) {
-			console.log(error);
-			return res.json({
-				success: false,
-				message: 'You need to input a value',
-			});
-		}
-	}
-	return res.json({ success: false, message: 'You need to input something' });
+      //Create new post
+      const newPost = new Post({
+        postText: post.postText,
+        postImage: filePath,
+        user: post.userID,
+      });
+      await newPost.save();
+      return res.json({
+        success: true,
+        message: "Post a status successfully",
+        newPost,
+      });
+    } catch (error) {
+      return res.json({
+        success: false,
+        message: "Cannot create a post",
+        error,
+      });
+    }
+  }
+  return res.json({ success: false, message: "You need to input something" });
 });
 
 //Get all post of an user
-router.get('/', verifyToken, async (req, res) => {
-	const { userID } = req.body;
-	const listOfPost = await Post.find({ user: userID });
-	res.json({ success: true, message: 'This is list of post', listOfPost });
+router.get("/", verifyToken, async (req, res) => {
+  const { userID } = req.body;
+  const listOfPost = await Post.find({ user: userID });
+  res.json({ success: true, message: "This is list of post", listOfPost });
 });
 
-router.put('/update/:id', verifyToken, async (req, res) => {
-	const postID = req.params.id;
-	const { userID, postText, email } = req.body;
+router.get("/:id", verifyToken, async (req, res) => {
+  const postID = req.params.id;
 
-	const Post = await Post.findOne({ _id: postID, user: userID });
-
-	if (!Post) {
-		res.json({
-			success: false,
-			message: 'You do not have permission to delete it',
-		});
-	}
-
-	const imgName = Post.postImage;
-	const file = req.files?.postImage;
-	var filePath = '';
-
-	try {
-		if (imgName !== '' && imgName.slice(-file?.name?.length) !== file?.name) {
-			fs.unlinkSync(`../client/public/assets/uploads/posts/${imgName}`);
-		}
-		if (file?.name !== undefined) {
-			filePath = email.slice(0, 5) + '_' + Date.now().toString() + file.name;
-			file.mv(`../client/public/assets/uploads/posts/${filePath}`, (err) => {
-				console.error(err);
-			});
-		}
-
-		//Update a post
-		const newPost = {
-			postText,
-			postImage: filePath,
-			user: userID,
-		};
-		const updatedPost = await Post.findOneAndUpdate(
-			{ _id: postID, user: userID },
-			newPost,
-			{ new: true }
-		);
-		return res.json({
-			success: true,
-			message: 'Update a status successfully',
-			updatedPost,
-		});
-	} catch (error) {
-		return res.json({
-			success: false,
-			message: 'Update fail',
-		});
-	}
+  const { userID } = req.body;
+  const aPost = await Post.findOne({ user: userID, postID });
+  res.json({ success: true, message: "This is list of post", aPost });
 });
 
-router.delete('/delete/:id', verifyToken, async (req, res) => {
-	console.log(req.body);
-	const postID = req.params.id;
-	console.log(postID);
+router.put("/update/:id", verifyToken, async (req, res) => {
+  const postID = req.params.id;
+  const { userID, postText, email } = req.body;
 
-	const { userID } = req.body;
-	try {
-		await Post.findOneAndDelete({ _id: postID, user: userID });
+  const updatePost = await Post.findOne({ _id: postID, user: userID });
 
-		return res.json({
-			success: true,
-			message: 'Delete a status successfully',
-		});
-	} catch (error) {
-		console.log(error);
-		return res.json({
-			success: false,
-			message: 'Delete fail',
-		});
-	}
+  if (!updatePost) {
+    res.json({
+      success: false,
+      message: "You do not have permission to delete it",
+    });
+  }
+
+  const imgName = updatePost.postImage;
+  const file = req.files?.postImage;
+  var filePath = "";
+
+  try {
+    if (imgName !== "" && imgName.slice(-file?.name?.length) !== file?.name) {
+      fs.unlinkSync(`../client/public/assets/uploads/posts/${imgName}`);
+    }
+    if (file?.name !== undefined) {
+      filePath = email.slice(0, 5) + "_" + Date.now().toString() + file.name;
+      file.mv(`../client/public/assets/uploads/posts/${filePath}`, (err) => {
+        console.error(err);
+      });
+    }
+
+    //Update a post
+    const newPost = {
+      postText,
+      postImage: filePath,
+      user: userID,
+    };
+    const updatedPost = await Post.findOneAndUpdate(
+      { _id: postID, user: userID },
+      newPost,
+      { new: true }
+    );
+    return res.json({
+      success: true,
+      message: "Update a status successfully",
+      updatedPost,
+    });
+  } catch (error) {
+    return res.json({
+      success: false,
+      message: "Update fail",
+      error,
+    });
+  }
+});
+
+router.delete("/delete/:id", verifyToken, async (req, res) => {
+  const postID = req.params.id;
+
+  const { userID } = req.body;
+  try {
+    const deletePost = await Post.findOneAndDelete({
+      _id: postID,
+      user: userID,
+    });
+
+    if (deletePost.postImage !== "") {
+      fs.unlinkSync(
+        `../client/public/assets/uploads/posts/${deletePost.postImage}`
+      );
+    }
+
+    return res.json({
+      success: true,
+      message: "Delete a status successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      success: false,
+      message: "Delete fail",
+    });
+  }
 });
 
 module.exports = router;
