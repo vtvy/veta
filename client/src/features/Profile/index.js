c6b1054d5bbba73ea88f64270f0f50f8c1362412
Vẽo CT <votrieuvy@votrieuvys-iMac.local>
2022-02-19 10:01:43 +0700

    fix vocabulary

--- client/src/features/Profile/index.js
+++ client/src/features/Profile/index.js
@@ -1,26 +1,26 @@
-import React, { useEffect } from 'react';
-import { Route, Routes, useNavigate, useParams } from 'react-router-dom';
-import userApi from '../../api/userApi';
-import ProfileInfo from './components/ProfileInfo';
-import ProfileNavbar from './components/ProfileNavbar';
-import Timeline from './Pages/Timeline';
+import React, { useEffect } from "react";
+import { Route, Routes, useNavigate, useParams } from "react-router-dom";
+import userApi from "../../api/userApi";
+import ProfileInfo from "./components/ProfileInfo";
+import ProfileNavbar from "./components/ProfileNavbar";
+import Timeline from "./Pages/Timeline";
 
 function Profile({ user }) {
-	let params = useParams();
-	const navigate = useNavigate();
-	useEffect(() => {
-		navigate(`/profile/${params.id}/timeline`);
-	}, []);
-	return (
-		<div className="w-full flex flex-col space-y-6 ">
-			<ProfileInfo user={user} />
-			<ProfileNavbar />
-			<Routes>
-				<Route path="/timeline" element={<Timeline />} />
-				<Route path="/photos" element={<Timeline />} />
-			</Routes>
-		</div>
-	);
+    let params = useParams();
+    const navigate = useNavigate();
+    useEffect(() => {
+        navigate(`/profile/${params.id}/timeline`);
+    }, []);
+    return (
+        <div className="w-full flex flex-col space-y-6 ">
+            <ProfileInfo user={user} />
+            <ProfileNavbar />
+            <Routes>
+                <Route path="/timeline" element={<Timeline />} />
+                <Route path="/photos" element={<Timeline />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default Profile;
