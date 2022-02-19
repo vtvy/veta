87e6282e9832543a7c98bf233a600b734c5f50f5
NguyenMinhNhat <nguyenminhnhat301101@gmail.com>
2022-02-19 10:15:45 +0700

    nothing

--- client/src/components/QuickViewUser/index.js
+++ client/src/components/QuickViewUser/index.js
@@ -4,7 +4,11 @@ import Avatar from '../../components/Avatar';
 
 function QuickViewUser({ user, showEmail, showFollower }) {
 	return (
-		<Link to={`/profile/${user._id}/timeline`} className="flex items-center">
+		<Link
+			to={`/profile/${user._id}/timeline`}
+			className="flex items-center 
+    "
+		>
 			<div className="flex-shrink-0">
 				<Avatar avatar={user.avatar} />
 			</div>
@@ -12,7 +16,7 @@ function QuickViewUser({ user, showEmail, showFollower }) {
 				<span className="text-2xl font-semibold">{user.name}</span>
 				{showEmail && <span className="text-lg">{user.email}</span>}
 				{showFollower && (
-					<span className="text-lg">{user.followers.length}</span>
+					<span className="text-lg">Follower: {user.followers.length}</span>
 				)}
 			</div>
 		</Link>
