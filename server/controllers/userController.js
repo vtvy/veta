85d4ac881fc03ad217b6eaa223eb721db699509d
votrieuvy1661 <votrieuvy1661@gmail.com>
2022-02-19 10:47:03 +0700

    finish some function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,72 +3,76 @@ const User = require("../models/User");
 var success = false;
 
 const UserController = {
-  search: async (req, res) => {
-    const name = req.params.name;
-    try {
-      var searchUser = await User.find({ name: { $regex: name } })
-        .limit(10)
-        .select("name avatar followers");
-      success = true;
-      res.json({
-        success,
-        message: "This is list of searching user",
-        searchUser,
-      });
-    } catch (error) {
-      console.log(error);
-      res.json({ success, message: "There are something wrong!", error });
-    }
-  },
+    search: async (req, res) => {
+        const name = req.params.name;
+        try {
+            var searchUser = await User.find({ name: { $regex: name } })
+                .limit(10)
+                .select("name avatar followers");
+            success = true;
+            res.json({
+                success,
+                message: "This is list of searching user",
+                searchUser,
+            });
+        } catch (error) {
+            console.log(error);
+            res.json({ success, message: "There are something wrong!", error });
+        }
+    },
 
-  follow: async (req, res) => {
-    const { userID, friendID } = req.body;
+    follow: async (req, res) => {
+        const { userID, friendID } = req.body;
 
-    try {
-      var myself = await User.findOne({ user: userID }).select("following");
-      var friend = await User.findOne({ user: friendID }).select("followers");
-      const notFollow = friend && !myself.following.includes(friendID);
-      var state = 0;
-      if (notFollow) {
-        //Follow
-        await User.findOneAndUpdate(
-          { user: userID },
-          { $push: { following: friendID } }
-        );
+        try {
+            var myself = await User.findOne({ user: userID }).select(
+                "following"
+            );
+            var friend = await User.findOne({ user: friendID }).select(
+                "followers"
+            );
+            const notFollow = friend && !myself.following.includes(friendID);
+            var state = 0;
+            if (notFollow) {
+                //Follow
+                await User.findOneAndUpdate(
+                    { user: userID },
+                    { $push: { following: friendID } }
+                );
 
-        friend = await User.findOneAndUpdate(
-          { user: friendID },
-          { $push: { followers: userID } },
-          { new: true }
-        ).select("name avatar followers");
+                friend = await User.findOneAndUpdate(
+                    { user: friendID },
+                    { $push: { followers: userID } },
+                    { new: true }
+                ).select("name avatar followers");
 
-        state = 1;
-      } else {
-        //Unfollow
-        await User.findOneAndUpdate(
-          { user: userID },
-          { $pull: { following: friendID } }
-        );
+                state = 1;
+            } else {
+                //Unfollow
+                await User.findOneAndUpdate(
+                    { user: userID },
+                    { $pull: { following: friendID } }
+                );
 
-        friend = await User.findOneAndUpdate(
-          { user: friendID },
-          { $pull: { followers: userID } },
-          { new: true }
-        ).select("name avatar followers");
-        state = -1;
-      }
-      success = true;
-    } catch (error) {
-      console.log(error);
-    }
+                friend = await User.findOneAndUpdate(
+                    { user: friendID },
+                    { $pull: { followers: userID } },
+                    { new: true }
+                ).select("name avatar followers");
+                state = -1;
+            }
+            success = true;
+        } catch (error) {
+            console.log(error);
+        }
 
-    if (req.files) await deleteTmp(req.files);
-    if (success) {
-      res.json({ success, message: "Successful action", friend, state });
-    } else {
-      res.json({ success, message: "May be something wrong" });
-    }
-  },
+        if (req.files) await deleteTmp(req.files);
+        if (success) {
+            res.json({ success, message: "Successful action", friend, state });
+        } else {
+            res.json({ success, message: "May be something wrong" });
+        }
+    },
 };
 
 module.exports = UserController;
